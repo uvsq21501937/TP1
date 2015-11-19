c85aa968675c2c7d8a8ab3bb7119034035a35dd9
uvsqEmmanuel <yarmanuel91@yahoo.fr>
2015-11-19 01:44:57 +0100

    2eme test effectue avec succes.

--- Ex3_1/src/main/java/fr/uvsq/coo/ex3_2/Game.java
+++ Ex3_1/src/main/java/fr/uvsq/coo/ex3_2/Game.java
@@ -9,11 +9,12 @@ package fr.uvsq.coo.ex3_2;
  * @author Yarmanuel
  */
 public class Game {
+    private int score=0;
     
     public void roll(int pins){
-        
+        score+=pins;
     }
     public int score(){
-        return 0;
+        return score;
     }
 }
--- Ex3_1/src/test/java/fr/uvsq/coo/ex3_2/BowlingGameTest.java
+++ Ex3_1/src/test/java/fr/uvsq/coo/ex3_2/BowlingGameTest.java
@@ -6,20 +6,36 @@ package fr.uvsq.coo.ex3_2;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.Before;
 
 /**
  *
  * @author Yarmanuel
  */
 public class BowlingGameTest {
+    private  Game g;
+    
+    @Before
+    public void setUp() throws Exception{
+        g=new Game();
+    }
+    
+    private void rollMany(int n, int pins) {
+        for (int i = 0; i < n; i++) g.roll(pins);
+    }
     
     @Test
     public void testGutterGame()throws Exception{
-        Game g= new Game();
-        for (int i=0;i<20; i++){
-            g.roll(0);
+        rollMany(20, 0);
             assertEquals(0, g.score());
         }
         
-    }
+    
+    @Test
+    public void testAllOnes ()throws Exception{
+        rollMany(20, 1);
+        assertEquals(20, g.score());
+}
+          
+ 
 }
