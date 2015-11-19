fb6f86ef2c91452b4117182bb09ce7817395c653
uvsqEmmanuel <yarmanuel91@yahoo.fr>
2015-11-19 11:55:39 +0100

    Changement du nom du projet, je l'avais nomme Ex3_1 alors que ca doit etre Ex3

---
+++ Ex_3/src/test/java/fr/uvsq/coo/ex3_1/AppTest.java
@@ -0,0 +1,21 @@
+package fr.uvsq.coo.ex3_1;
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    @Test
+    public void AppTest(  )
+    {
+        assert true;
+    }
+   
+}
---
+++ Ex_3/src/test/java/fr/uvsq/coo/ex3_2/BowlingGameTest.java
@@ -0,0 +1,75 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.uvsq.coo.ex3_2;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.Before;
+
+/**
+ *
+ * @author Yarmanuel
+ */
+public class BowlingGameTest {
+    private  Game g;
+    
+    @Before
+    public void setUp() throws Exception{
+        g=new Game();
+    }
+    
+    private void rollMany(int n, int pins) {
+        for (int i = 0; i < n; i++) g.roll(pins);
+    }
+    
+    @Test
+    public void testGutterGame()throws Exception{
+        rollMany(20, 0);
+            assertEquals(0, g.score());
+        }
+        
+    
+    @Test
+    public void testAllOnes ()throws Exception{
+        rollMany(20, 1);
+        assertEquals(20, g.score());
+}
+     
+    @Test
+    public void testOneSpare() throws Exception {
+            rollSpare();
+            g.roll(3);
+            rollMany(17,0);
+            assertEquals(16,g.score());
+  }
+    
+    @Test
+     public void testOneStrike() throws Exception {
+            rollStrike();
+            g.roll(3);
+            g.roll(4);
+            rollMany(16, 0);
+            assertEquals(24, g.score());
+  }
+    
+ @Test
+   public void testPerfectGame() throws Exception {
+    rollMany(12,10);
+    assertEquals(300, g.score());
+  }
+ 
+ 
+  private void rollSpare() {
+    g.roll(5);
+    g.roll(5);
+  }
+  
+   private void rollStrike() {
+    g.roll(10); 
+  }
+   
+   
+    
+}
