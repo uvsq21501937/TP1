df98ba79ba47279218df6f3e5b90feaebbc6b960
uvsqEmmanuel <yarmanuel91@yahoo.fr>
2015-11-19 12:14:53 +0100

    EXO3_3 avec mise en place du principe SRP

---
+++ Ex_3/src/main/java/fr/uvsq/coo/ex3_3/Affichage.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.uvsq.coo.ex3_3;
+
+/**
+ *
+ * @author Yarmanuel
+ */
+public class Affichage {
+    
+    public static void afficheCoordonees(String coordonnee){
+            System.out.println(coordonnee);
+    }
+}
---
+++ Ex_3/src/main/java/fr/uvsq/coo/ex3_3/Employe.java
@@ -0,0 +1,26 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.uvsq.coo.ex3_3;
+
+/**
+ *
+ * @author Yarmanuel
+ */
+public class Employe {
+    private final String name;
+    private final String adresse;
+    
+    public Employe(){
+       name= " ";
+       adresse = " ";
+    }
+    
+    public String getCoordonee(){
+        return name+" "+adresse;
+    }
+    public double calculSalaire(){
+        return 0.0;
+    }
+}
