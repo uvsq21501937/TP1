e2dcf9713454988d13ac16ee12b2070f0eb12907
uvsqEmmanuel <yarmanuel91@yahoo.fr>
2015-11-19 01:04:33 +0100

    Mise a jour de la version de JUnit a 4.12 et modification du code de test par defaut. Nous avons effectuer le test avec mvn test, et ca marche.

--- Ex3_1/src/test/java/fr/uvsq/coo/ex3_1/AppTest.java
+++ Ex3_1/src/test/java/fr/uvsq/coo/ex3_1/AppTest.java
@@ -1,38 +1,21 @@
 package fr.uvsq.coo.ex3_1;
-
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.Test;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest 
-    extends TestCase
+
 {
     /**
      * Create the test case
      *
      * @param testName name of the test case
      */
-    public AppTest( String testName )
+    @Test
+    public void AppTest(  )
     {
-        super( testName );
+        assert true;
     }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+   
 }
